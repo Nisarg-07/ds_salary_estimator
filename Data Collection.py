ca959da326c2a5a296d9b2b8062791d65202d243
Nisarg-07 <pnick714@gmail.com>
2024-07-03 10:45:39 -0400

    uploaded scarpper and run code

---
+++ Data Collection.py
@@ -0,0 +1,8 @@
+import Glassdoor_Scrapper as gs
+import pandas as pd
+
+path = "chromedriver.exe"
+
+df = gs.get_jobs("data scientist", 1000, False, path, 15)
+
+df.to_csv("glassdoor_jobs.csv", index=False)
